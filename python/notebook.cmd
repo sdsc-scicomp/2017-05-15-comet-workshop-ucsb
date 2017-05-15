aa84ecd61fdfa85e2aa22e403b71136f6d1ec98d
Andrea Zonca <code@andreazonca.com>
2017-05-15 08:25:21 -0700

    python spark

---
+++ python/notebook.cmd
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH --job-name="jupyter-notebook"
+#SBATCH --output="jupyter-notebook.%j.%N.out"
+#SBATCH --partition=compute
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=24
+#SBATCH --export=ALL
+#SBATCH --reservation=UCSBRes
+#SBATCH -t 04:00:00
+
+export MODULEPATH=/share/apps/compute/modulefiles/applications:$MODULEPATH
+module load anaconda
+jupyter notebook --no-browser --ip=*
